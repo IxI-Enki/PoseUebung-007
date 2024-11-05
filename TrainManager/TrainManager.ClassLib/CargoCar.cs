99e683dbb2da2b1bb7ba5ae6e9123da39c7b5747
Enki <cell.enohp.apps@gmail.com>
2024-11-05 13:05:10 +0100

    init classlib

---
+++ TrainManager/TrainManager.ClassLib/CargoCar.cs
@@ -0,0 +1,24 @@
+﻿namespace TrainManager.ClassLib;
+
+internal class CargoCar
+{
+  #region FIELDS
+
+  #endregion
+
+  #region PROPERTIES
+
+  #endregion
+
+  #region CONSTRUCTOR
+
+  #endregion
+
+  #region METHODS
+
+  #endregion
+
+  #region OVERRIDES
+
+  #endregion
+} 
---
+++ TrainManager/TrainManager.ClassLib/Carriage.cs
@@ -0,0 +1,24 @@
+﻿namespace TrainManager.ClassLib;
+
+public class Carriage
+{
+  #region FIELDS
+
+  #endregion
+
+  #region PROPERTIES
+
+  #endregion
+
+  #region CONSTRUCTOR
+
+  #endregion
+
+  #region METHODS
+
+  #endregion
+
+  #region OVERRIDES
+
+  #endregion
+}
